7f02a21705ddba0c25378d3b3968077abc0c430e
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2019-05-16 03:09:28 +0430

    added Clinic and TimeSlot models

--- cleanq/clinic/models.py
+++ cleanq/clinic/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from users.models import ClinicRepresentative, BasicUser
 
-# Create your models here.
+import datetime
+from django.utils import timezone
+
+# settings.AUTH_USER_MODEL
+
+
+class Clinic(models.Model):
+    rep = models.ForeignKey(
+        ClinicRepresentative,
+        help_text=_("representative of the clinic"),
+        verbose_name=_("rep"),
+        on_delete=models.SET_NULL,
+    )
+
+
+class TimeSlot(models.Model):
+    clinic = models.ForeignKey(
+        Clinic,
+        help_text=_("the clinic that this time slot belongs to"),
+        verbose_name=_("clinic"),
+        on_delete=models.CASCADE,
+    )
+    start_time = models.DateTimeField("start time", default=timezone.now, blank=True)
+    end_time = models.DateTimeField(
+        "end time", default=timezone.now + datetime.timedelta(hours=3), blank=True
+    )
+
+    reserver = models.ForeignKey(
+        BasicUser,
+        help_text=_("representative of the clinic"),
+        verbose_name=_("rep"),
+        on_delete=models.SET_NULL,
+        default=None,
+        null=True,
+        blank=True,
+    )
+
